60a6fd27256dd0159f194fed0be5def74c6222b2
JeGDeSaN <145546683+JeGDeSaN@users.noreply.github.com>
2024-01-19 19:16:07 +0530

    Add files via upload

---
+++ assignmnt10/hofNcbf.js
@@ -0,0 +1,10 @@
+function callbackFunction(){
+    console.log('I am  a callback function');
+}
+
+function higherOrderFunction(func){
+    console.log('I am higher order function')
+    func()
+}
+
+higherOrderFunction(callbackFunction);
